fa1fa9fe55e74b2020b75ba70462a7300fa11afa
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2024-05-26 18:23:48 +0530

    Minor updates

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,9 @@
 import uvicorn
 
 from api.utils.logger import logger
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from api.utils.utils import read_config
 from api.routes import files_router, query_router, ingest_router
-from api.src.qna import QnA
-from api.src.ingestion import Ingestion
 
 app = FastAPI()
 app.include_router(files_router)
@@ -20,7 +18,7 @@ async def home():
 
 
 if __name__ == "__main__":
-    logger.info({"message": f"FastAPI running on PORT {config['PORT']}"})
+    logger.info(f"FastAPI running on PORT {config['PORT']}")
     uvicorn.run(
         app="app:app",
         host=config["HOST"],
